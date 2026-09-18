5af8c4c7a5ed4bd3aabd4ade26f93b4f274a42c8
”josueosorio13826” <00013826@uca.edu.sv>
2026-09-18 00:25:28 -0600

    feat: agregar modelado y validacion de elementos

--- parcial.cpp
+++ parcial.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+struct Elemento {
+    string codigo;
+    string nombre;
+    float longitud;
+    float cargas[3];
+    float capacidadMaxima;
+    float factorUtilizacion;
+    string estadoSeguridad;
+};
 int main () {
 
+    Elemento elementos[10];
+    int cantidad;
 
+    do {
+        cout << "Cuantos elementos desea registrar (1-10): ";
+        cin >> cantidad;
 
-return 0;
+        if (cantidad < 1 || cantidad > 10) {
+            cout << "Cantidad invalida. Intente nuevamente." << endl;
+        }
+
+    } while (cantidad < 1 || cantidad > 10);
+
+    cout << "Cantidad registrada correctamente: " << cantidad << endl;
+
+    return 0;
 }
+
+
+
