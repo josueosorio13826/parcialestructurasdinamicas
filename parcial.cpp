4c59ac4edcf4269dab78666784d3c4eb28a96ae9
”josueosorio13826” <00013826@uca.edu.sv>
2026-09-18 01:33:03 -0600

    feat: calcular factor de utilizacion con punteros

--- parcial.cpp
+++ parcial.cpp
@@ -13,6 +13,7 @@ struct Elemento {
 };
 
 void registrarElemento(Elemento &elemento);
+float calcularFactor(Elemento *elemento);
 
 int main () {
 
@@ -36,7 +37,18 @@ int main () {
 }
 
 cout << "\nRegistro completado correctamente." << endl;
+Elemento *puntero = elementos;
 
+    cout << "\n--- FACTORES DE UTILIZACION ---" << endl;
+
+    for (int i = 0; i < cantidad; i++) {
+        float factor = calcularFactor(puntero);
+
+        cout << "Elemento: " << puntero->nombre << endl;
+        cout << "Factor de utilizacion: " << factor << endl;
+
+        puntero++;
+    }
     return 0;
 }
 
@@ -62,6 +74,21 @@ cout << "Codigo: ";
 
     elemento.factorUtilizacion = 0;
     elemento.estadoSeguridad = "SIN CALCULAR";
+}
+
+float calcularFactor(Elemento *elemento) {
+    float sumaCargas = 0;
+
+    for (int i = 0; i < 3; i++) {
+        sumaCargas += elemento->cargas[i];
+    }
+
+    float cargaPromedio = sumaCargas / 3.0;
+
+    elemento->factorUtilizacion =
+        cargaPromedio / elemento->capacidadMaxima;
+
+    return elemento->factorUtilizacion;
 }
 
 
